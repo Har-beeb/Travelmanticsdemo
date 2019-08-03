a08739a6c7bd839a3d094f3d5066847851749f11
Har-beeb <habeeb.finex@gmail.com>
2019-08-03 15:18:12 +0100

    fixed bug

--- app/src/main/java/com/example/travelmanticsdemo/ListActivity.java
+++ app/src/main/java/com/example/travelmanticsdemo/ListActivity.java
@@ -3,6 +3,8 @@ package com.example.travelmanticsdemo;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.TextView;
@@ -25,41 +27,42 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-        mFirebaseDatabase = FirebaseDatabase.getInstance();
-        mDatabaseReference = mFirebaseDatabase.getReference().child("traveldeals");
-        mChildListener = new ChildEventListener() {
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                TextView txtViewDeal = findViewById(R.id.txtViewDeal);
-                TravelDeal travelDeal = dataSnapshot.getValue(TravelDeal.class);
-                txtViewDeal.setText(txtViewDeal.getText() + "\n" + travelDeal.getTitle());
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        };
+//        mFirebaseDatabase = FirebaseDatabase.getInstance();
+//        mDatabaseReference = mFirebaseDatabase.getReference().child("traveldeals");
+//        mChildListener = new ChildEventListener() {
+//            @Override
+//            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+//                TextView txtViewDeal = findViewById(R.id.textView);
+//                TravelDeal travelDeal = dataSnapshot.getValue(TravelDeal.class);
+//                txtViewDeal.setText(txtViewDeal.getText() + "\n" + travelDeal.getTitle());
+//            }
+//
+//            @Override
+//            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+//
+//            }
+//
+//            @Override
+//            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+//
+//            }
+//
+//            @Override
+//            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        };
         mDatabaseReference.addChildEventListener(mChildListener);
-//        RecyclerView recyclerView = findViewById(R.id.list_item);
-//        final DealAdapter dealAdapter = new DealAdapter();
-//        recyclerView.setAdapter(dealAdapter);
-//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
-//        recyclerView.setLayoutManager(linearLayoutManager);
+        RecyclerView recyclerView = findViewById(R.id.list_item);
+        final DealAdapter dealAdapter = new DealAdapter();
+        recyclerView.setAdapter(dealAdapter);
+        LinearLayoutManager linearLayoutManager =
+                new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
     }
 }
